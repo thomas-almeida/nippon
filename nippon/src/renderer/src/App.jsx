7aa9379a84bbc345b8238d732f4e7a14cda2f7b5
ThomRodrigues <thomas.rodrigues@idealtrends.com.br>
2023-05-26 01:08:17 -0300

    criando projeto

---
+++ nippon/src/renderer/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <>
+      <h1>Nippon</h1>
+    </>
+  )
+}
+
+export default App
---
+++ nippon/src/renderer/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './assets/main.css'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
