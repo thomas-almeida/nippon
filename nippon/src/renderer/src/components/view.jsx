44f21e5305c164315a58f31ca7a92a53639a1e88
ThomRodrigues <thomas.rodrigues@idealtrends.com.br>
2023-05-26 22:07:21 -0300

    ações de visualização

--- nippon/src/renderer/src/components/view.jsx
+++ nippon/src/renderer/src/components/view.jsx
@@ -1,11 +1,36 @@
-import { IoEyeOutline, IoCopyOutline, IoBookmarkOutline } from 'react-icons/io5';
+import { IoEyeOutline, IoCopyOutline, IoBookmarkOutline, IoEyeOffOutline } from 'react-icons/io5';
 import Editor from './MarkDownEditor';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ReactMarkdown from 'react-markdown'
 import Prism from 'prismjs'
 import 'prismjs/themes/prism-tomorrow.css';
 
 export function View() {
+
+    const editorFieldRef = useRef()
+    const previewFieldRef = useRef()
+    const eyeRef = useRef()
+
+
+    function showPreview() {
+        if (previewFieldRef.current.style.display != 'block') {
+            editorFieldRef.current.style.display = 'none'
+            previewFieldRef.current.style.display = 'block'
+            previewFieldRef.current.style.width = '100%'
+        } else {
+            previewFieldRef.current.style.display = 'none'
+            editorFieldRef.current.style.display = 'block'
+            editorFieldRef.current.style.width = '100%'
+        }
+    }
+
+    function split() {
+        editorFieldRef.current.style.display = 'block'
+        previewFieldRef.current.style.display = 'block'
+
+        previewFieldRef.current.style.width = '50%'
+        editorFieldRef.current.style.width = '50%'
+    }
 
     useEffect(() => {
         Prism.highlightAll()
@@ -27,15 +52,15 @@ export function View() {
             <div className="view">
                 <h4>file name</h4>
                 <section className="actions">
-                    <IoEyeOutline />
-                    <IoCopyOutline />
+                    <IoEyeOutline ref={eyeRef} onClick={showPreview} />
+                    <IoCopyOutline onClick={split} />
                     <IoBookmarkOutline />
                 </section>
                 <div className="editor-preview">
-                    <div className='editor'>
+                    <div className='editor' ref={editorFieldRef}>
                         <Editor markdown={markdown} change={handleEditorChange} />
                     </div>
-                    <div className='preview'>
+                    <div className='preview' ref={previewFieldRef}>
                         <ReactMarkdown renderers={renderers}>{markdown}</ReactMarkdown>
                     </div>
                 </div>
