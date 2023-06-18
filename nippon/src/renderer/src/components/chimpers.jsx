b80bcf8d9ac88b0db14204be0fcd91a2a51c431a
Thomas Almeida <contato.thomasalmeidard@gmail.com>
2023-06-17 21:47:03 -0300

    novos chimpers

--- nippon/src/renderer/src/components/chimpers.jsx
+++ nippon/src/renderer/src/components/chimpers.jsx
@@ -1,3 +1,16 @@
-let chimpers = ['https://public.nftstatic.com/static/nft/webp/nft-cex/S3/1670005026631_0me2vqk7fqy4hujdwj0q1gr4ydpmkhv7_600x600.webp', 'https://public.nftstatic.com/static/nft/webp/nft-cex/S3/1676540231511_wezhfil9d59vvp4e6bthttzlc4tl0rrj_600x600.webp', 'https://public.nftstatic.com/static/nft/webp/nft-cex/S3/1672252688476_7lqruc0z1ug6w1wh3zm58kl4vrftoobn_600x600.webp', 'https://public.nftstatic.com/static/nft/webp/nft-cex/S3/1677723849223_48r743majphdtv525txp9mwzrnx97piw_600x600.webp', 'https://public.nftstatic.com/static/nft/webp/nft-cex/S3/1676716092166_brma8o8t1hqaersd8lpagirrrklfqh58_600x600.webp', 'https://public.nftstatic.com/static/nft/res/nft-cex/S3/1671355627961_p73r0zb5opjc6ba52w3szi3kqv735kcw.png', 'https://public.nftstatic.com/static/nft/res/nft-cex/S3/1672263915146_1skib1r7j4n707ma05xzumviqc5nuc7h.png', 'https://public.nftstatic.com/static/nft/res/nft-cex/S3/1667667056138_uc2ghasougt94gegbc1ntv9ed8a0a1lr.png']
+let chimpers = [
+'https://public.nftstatic.com/static/nft/webp/nft-cex/S3/1670005026631_0me2vqk7fqy4hujdwj0q1gr4ydpmkhv7_600x600.webp',
+'https://public.nftstatic.com/static/nft/webp/nft-cex/S3/1676540231511_wezhfil9d59vvp4e6bthttzlc4tl0rrj_600x600.webp',
+'https://public.nftstatic.com/static/nft/webp/nft-cex/S3/1672252688476_7lqruc0z1ug6w1wh3zm58kl4vrftoobn_600x600.webp', 
+'https://public.nftstatic.com/static/nft/webp/nft-cex/S3/1677723849223_48r743majphdtv525txp9mwzrnx97piw_600x600.webp', 
+'https://public.nftstatic.com/static/nft/webp/nft-cex/S3/1676716092166_brma8o8t1hqaersd8lpagirrrklfqh58_600x600.webp', 
+'https://public.nftstatic.com/static/nft/res/nft-cex/S3/1671355627961_p73r0zb5opjc6ba52w3szi3kqv735kcw.png', 
+'https://public.nftstatic.com/static/nft/res/nft-cex/S3/1672263915146_1skib1r7j4n707ma05xzumviqc5nuc7h.png', 
+'https://public.nftstatic.com/static/nft/res/nft-cex/S3/1667667056138_uc2ghasougt94gegbc1ntv9ed8a0a1lr.png',
+'https://public.nftstatic.com/static/nft/webp/nft-cex/S3/1668867008650_h043jqyoo43yq8as6f970uieo8dpdfmi_600x600.webp',
+'https://public.nftstatic.com/static/nft/webp/nft-cex/S3/1686540074021_u0ej5btx177g6m5sixtncpitsrhxlx4t_600x600.webp',
+'https://public.nftstatic.com/static/nft/webp/nft-cex/S3/1670267532063_372hodjgbib02masoha1jm7i5oi4iwgx_600x600.webp',
+'https://public.nftstatic.com/static/nft/webp/nft-cex/S3/1686217812863_uo06401z2v7wrx9pocvjeap369er8t9e_600x600.webp'
+]
 
 export default chimpers
