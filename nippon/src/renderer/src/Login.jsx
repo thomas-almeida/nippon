1b6b4fe8f5be4145399aa8e0e2e924a08a9c5a23
thomas-almeida <thomas.almeida@tradeupgroup.com>
2023-12-19 18:50:03 -0300

    begining login screen

---
+++ nippon/src/renderer/src/Login.jsx
@@ -0,0 +1,11 @@
+export default function Login() {
+    return (
+        <>
+            <div className='config-container' id='config-screen'>
+                <div className='config-content'>
+                    <h1>AAAA</h1>
+                </div>
+            </div>
+        </>
+    )
+}
--- nippon/src/renderer/src/main.jsx
+++ nippon/src/renderer/src/main.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './assets/main.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import './assets/main.css';
+import App from './App';
+import Login from './Login';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+const isUserLoggedIn = () => {
+  return false;
+};
+
+const Main = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={isUserLoggedIn() ? <App /> : <Navigate to="/login" />}
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Main />);
