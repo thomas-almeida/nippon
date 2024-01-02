2a5f18485f0bad08ac4ea78110be8e111d4ea11d
thomas-almeida <thomas.almeida@tradeupgroup.com>
2024-01-02 17:16:35 -0300

    vuetify-electron-vue template

---
+++ nippon-lts/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <h1>Hello</h1>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({
+
+  }),
+}
+</script>
---
+++ nippon-lts/src/views/LoginView.vue
@@ -0,0 +1,18 @@
+<template>
+  <LoginForm />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+// Components
+import LoginForm from "../components/LoginForm.vue";
+
+export default defineComponent({
+  name: "LoginView",
+
+  components: {
+    LoginForm,
+  },
+});
+</script>
